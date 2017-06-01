53954da500e6c1882c0c813d74b98927b3b2fd8e
ArkBeer <breaker0717@gmail.com>
2017-06-02 05:23:33 +0900

    gene

--- gene.cpp
+++ gene.cpp
@@ -5,90 +5,125 @@
 Ark::FrameRate fps;
 Ark::Random rnd;
 Ark::Key key;
-const int w_x = 50, w_y = 50;
+const int w_x = 50, w_y = 50,num=100,gene_num=20;
 
-class poyo {
-	int life;
+class pyo {
 	std::vector<int> gene;
-	int gcount;
 	int x, y;
-	const auto& makegene(std::vector<int> vec1, std::vector<int> vec2) {
-
-		std::vector<int> vec;
-		const int max = vec1.size() >= vec2.size() ? vec1.size() : vec2.size();
-		const int min = vec1.size() <= vec2.size() ? vec1.size() : vec2.size();
-		for (int i = min; i < max;++i) {
-			if (vec1.size() != max) {
-				vec1.push_back(rnd.Range(0, 4));
-			}
-			else vec2.push_back(rnd.Range(0, 4));
-		}
-		for (int i = 0; i < max;++i) {
-			vec.push_back(vec1.at(i) == vec2.at(i) ? vec1.at(i) : (rnd.Range(0, 1) ? vec1.at(i):vec2.at(i)));
-		}
-		return vec;
+	int count;
+	int child;
+	std::vector<int> rndgene(const int gene_num) {
+		std::vector<int> _gene;
+		for (int i = 0; i < gene_num; ++i)_gene.push_back(rnd.Range(0, 4));
+		return _gene; 
 	}
-	void randomize() {
-		life = life*(rnd.Range(0, 10) + 5) / 10;
-		for (auto& v:gene) {
-			if (rnd.Range(1, 10) == 1)v = rnd.Range(0, 4);
+	std::vector<int> makegene(const std::vector<int>& v1,const std::vector<int>& v2){
+		std::vector<int> v;
+		for (int i = 0; i < v1.size();++i) {
+			if (rnd.Range(1, 10) != 1) {
+				v.push_back(rnd.Range(0, 1) ? v1.at(i) : v2.at(i));
+			}
+			else v.push_back(rnd.Range(0, 4));
 		}
+		return v;
 	}
 public:
-	poyo(const std::vector<int>& _gene, const int _life, const int _x,const int _y):life(_life),gene(_gene), gcount(0),x(_x),y(_y) {
-	
-	}
-	poyo() :poyo(std::vector<int>{rnd.Range(0,4), rnd.Range(0, 4), rnd.Range(0, 4), rnd.Range(0, 4), rnd.Range(0, 4)},5,rnd.Range(0,w_x-1), rnd.Range(0,w_y-1)) {}
+	pyo(const std::vector<int>& _gene, const int _x,const int _y):gene(_gene),x(_x),y(_y),count(0){}
+	pyo(const std::vector<int>& _gene) :pyo(_gene, rnd.Range(0, w_x), rnd.Range(0, w_y)) {}
+	pyo() :pyo(rndgene(gene_num)) {}
+	pyo(const pyo& p1,const pyo& p2):pyo(makegene(p1.gene,p2.gene)) {}
 	void move() {
-		if (gcount!=gene.size()) {
-			switch (gene.at(gcount)) {
-			case 0:
+		if (count!=gene.size()) {
+			switch (gene.at(count)) {
+			case 1://left
+				x != 0 ? x -= 1 : x = w_x;
 				break;
-			case 1:		//left
-				x != 0 ? --x : x = w_x;
+			case 2://right
+				x != w_x ? x += 1 : x = 0;
 				break;
-			case 2:		//right
-				x != w_x ? ++x : x = 0;
+			case 3://up
+				y != 0 ? x -= 1 : y = w_y;
 				break;
-			case 3:		//up
-				y != 0 ? --y : y = w_y;
-				break;
-			case 4:		//down
-				y != w_y ? ++y : y = 0;
+			case 4://down
+				y != w_y ? y += 1 : y = 0;
 				break;
 			}
-			++gcount;
+			++count;
 		}
 	}
 	const int getx() { return x; }
 	const int gety() { return y; }
-	poyo& operator+(const poyo& r) {
-		poyo p(makegene(this->gene,r.gene),rnd.Range(this->life,r.life),this->x,this->y);
-		if (rnd.Range(1, 10)==1) {
-			p.randomize();
-		}
-		return p;
-	}
+	const bool is_dead() { return count == gene.size(); }
+	friend Ark::TstringStream& operator<<(Ark::TstringStream& ost, const pyo& p);
+
 };
+	Ark::TstringStream& operator<<(Ark::TstringStream& ost, const pyo& p) {
+		ost << "{";
+		for (auto&v:p.gene) {
+			ost << v;
+		}
+		ost << "}";
+		return ost;
+	}
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
 	Ark::WinClass wc(hInstance);
 	wc.AddStyle(WS_THICKFRAME).SetTitle(_T("Title")).SetSize(300,200);
 	Ark::D2D1_1 d;
-	poyo p1,p2;
+	
+	std::vector<pyo> p1;
+	std::vector<pyo> p2;
+	for (int i = 0; i < num; ++i)p1.push_back(pyo());
+	int counter=0;
 	while (!wc.EndFlag()) {
 		auto s = std::chrono::steady_clock::now();
 		wc.LockAspectRatio(10, 16);
 		auto rect=wc.GetSize();
-		if (key.KeyCheck('Z', false))p1.move(), p2.move();
+		//if (key.KeyCheck('Z', false)) {
+			for (auto&v:p1) {
+				v.move();
+			}
+			for (auto it1 = p1.begin(); it1 != p1.end()-1;++it1) {
+				for (auto it2 = it1 + 1; it2 != p1.end();++it2) {
+					if (it1->getx() == it2->getx() && it1->gety() == it2->gety() && !it1->is_dead() && !it2->is_dead()) {
+						p2.push_back(pyo(*it1, *it2));
+						p2.push_back(pyo(*it1, *it2));
+
+					}
+				}
+			}
+			if (p1.at(0).is_dead()) {
+				Ark::TstringStream tstr;
+				tstr << p2.size()<<"\n";
+				OutputDebugString(tstr.str().c_str());
+				++counter;
+				p1 = p2;
+				p2.clear();
+				if (p1.size() - 1 < num) {
+					for (int i = p1.size(); i < num; ++i) p1.push_back(pyo{});
+				}
+			}
+		//}
 		d.BeginDraw(wc.GethWnd());
 		d.DrawClear(RGB(0,66,66));
-		d.DrawRectangle(p1.getx()*rect.bottom / 50, p1.gety()*rect.bottom/50, p1.getx()*rect.bottom / 50 + rect.bottom / 50, p1.gety()*rect.bottom / 50 + rect.bottom / 50, RGB(255, 0, 0), 1.0f);
-		d.DrawRectangle(p2.getx()*rect.bottom / 50, p2.gety()*rect.bottom/50, p2.getx()*rect.bottom / 50 + rect.bottom / 50, p2.gety()*rect.bottom / 50 + rect.bottom / 50, RGB(0, 255, 0), 1.0f);
+
+		Ark::TstringStream list;
+		int c=1;
+		for (auto&v:p1) {
+			d.DrawRectangle(v.getx()*rect.bottom/50, v.gety()*rect.bottom / 50, (v.getx()+1)*rect.bottom / 50, (v.gety()+1)*rect.bottom / 50,RGB(0,250,150),1.0f);
+			list << v;
+			if (c%3== 0)list << "\n";
+			++c;
+		}
+		d.DrawString(rect.bottom,0,list.str().c_str(),RGB(0,0,0),1.0f,rect.bottom/num*3);
+		Ark::TstringStream countstr;
+		countstr << counter;
+		d.DrawString(0,0,countstr.str().c_str(),RGB(255,255,255),0.7f,rect.bottom/num*3);
 		d.EndDraw();
 		fps.Count();
 		Ark::TstringStream tstr;
-		tstr << fps.perDuration<double>(s);
+		//tstr << fps.perDuration<double>(s)<<"\n";
+		OutputDebugString(tstr.str().c_str());
 	}
 	return 0;
 }
